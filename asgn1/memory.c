e45cbe9952e85d0ddf7c660f33446ccbb8836625
batman <nernest@ucsc.edu>
2023-01-18 15:27:11 -0800

    Fixed get with quinn's classroom explanations

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -5,20 +5,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define BUFFER 4096
 
 int main() {
     // Input takes in the mode (get/set) and file name
-    char input[PATH_MAX + 4]; // PATH_MAX defined as the file name size limit
-    scanf("%[^\n]", input);
-
-    // Split the input string into mode and file name
-    char *mode = strtok(input, " ");
-    char *file_name = strtok(NULL, " ");
+    char mode[3]; // PATH_MAX defined as the file name size limit
+    char file_name[PATH_MAX];
+    scanf("%s %s", mode, file_name);
 
     // Opening the file
-    FILE *get = fopen(file_name, "r");
-    if (get == NULL) {
-        printf("File does not exist\n");
+    int get = open(file_name, O_RDONLY);
+    if (get < 0) {
+        printf("Invalid Filename\n");
         return 1;
     }
 
@@ -34,19 +36,38 @@ int main() {
     // If the entered command was get
     else if (strcmp(mode, "get") == 0) {
         // The buffer stores each line of the file temporarily.
-        char buffer[1024];
+        char buffer[BUFFER];
+        int bytes_read = 0;
+
+        do {
+            bytes_read = read(get, buffer + bytes_read, BUFFER); //Tested adding buffer and bytes_read
+            if (bytes_read < 0) {
+                fprintf(stderr, "Failed to read: %d\n", errno);
+                return 1;
+            }
+
+            else if (bytes_read > 0) {
+
+                int bytes_written = 0;
+                do {
+                    int bytes = write(STDOUT_FILENO, buffer + bytes_written, bytes_read - bytes_written); // Fix the same problem in read
+                    if (bytes <= 0) {
+                        fprintf(stderr, "Cannot write to STDOUT\n");
+			return 1;
+                    }
+
+                    bytes_written += bytes;
+                } while (bytes_written < bytes_read);
+            }
+        } while (bytes_read > 0);
 
         // Reads each line of the file and stores it in buffer.
         // Buffer is then printed
-        while (fscanf(get, "%[^\n] ", buffer) != EOF) {
-            printf("%s\n", buffer);
-        }
     } else {
         fprintf(stderr, "Invalid Command\n");
         return 1;
     }
 
     // No memory leaks?
-    fclose(get);
     return 0;
 }
