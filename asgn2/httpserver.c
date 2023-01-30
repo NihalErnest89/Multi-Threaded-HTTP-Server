a051cbddf222bef199b80ff374d2491392d9986e
batcomputer <nernest@ucsc.edu>
2023-01-30 14:05:09 -0800

    Added helper functions and started to work on sockets. Some reason, listener_Init is returning -1 perpetually

--- asgn2/httpserver.c
+++ asgn2/httpserver.c
@@ -3,13 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
+#include "asgn2_helper_funcs.h"
+
+int custom_error(int num) {
+    if (num == 1) {
+        fprintf(stderr, "Invalid Port\n");
+    }
+    return 0;
+}
 
 int main(int argc, char **argv) {
 
     // Throw an error if the number of command-line arguments is incorrect or the port number is not a number
     if (argc != 2) {
-       fprintf(stderr, "Invalid Port\n");
-       return 1;
+        custom_error(1);
+        return 1;
     }
 
     // Write a for loop that uses isdigit() to check if the entered port number is in fact a number.
@@ -25,10 +33,21 @@ int main(int argc, char **argv) {
     int port = atoi(argv[1]);
 
     if (is_number == 0 || port < 1 || port > 65535) {
-	    fprintf(stderr, "Invalid Port\n");
+	    custom_error(1);
 	    return 1;
     }
 
-    printf("Port number: %d\n", port); 
+    printf("Port Number: %d\n", port);
+
+    Listener_Socket sock;
+    int li = listener_init(&sock, port);
+
+    if (li < 0) {
+	printf("li = %d\n", li);
+        custom_error(1);
+	return 1;
+    }
+
+
     return 0;
 }
